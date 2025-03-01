ff4aaf5608d0a79db2d3d9140e5201926713633e
rjswjddn <rjswjddn@gmail.com>
2025-03-01 23:01:24 +0900

    fix(DB-208): 리뷰 별점 보기 별점 계산 수정

--- app-external-api/src/main/java/com/dongsan/api/domains/review/GetWalkwayRatingResponse.java
+++ app-external-api/src/main/java/com/dongsan/api/domains/review/GetWalkwayRatingResponse.java
@@ -19,11 +19,11 @@ public record GetWalkwayRatingResponse(
                 ? Math.round(RatingCalculator.calculateAverageRating(ratingCounts) * 10.0) / 10.0
                 : 0.0;
 
-        Long fiveRate = ratingCounts.getOrDefault(5, 0L) * 100 / totalReviewCount;
-        Long fourRate = ratingCounts.getOrDefault(4, 0L) * 100 / totalReviewCount;
-        Long threeRate = ratingCounts.getOrDefault(3, 0L) * 100 / totalReviewCount;
-        Long twoRate = ratingCounts.getOrDefault(2, 0L) * 100 / totalReviewCount;
-        Long oneRate = ratingCounts.getOrDefault(1, 0L) * 100 / totalReviewCount;
+        Long fiveRate = totalReviewCount == 0.0 ? 0L : ratingCounts.getOrDefault(5, 0L) * 100 / totalReviewCount;
+        Long fourRate = totalReviewCount == 0.0 ? 0L : ratingCounts.getOrDefault(4, 0L) * 100 / totalReviewCount;
+        Long threeRate = totalReviewCount == 0.0 ? 0L : ratingCounts.getOrDefault(3, 0L) * 100 / totalReviewCount;
+        Long twoRate = totalReviewCount == 0.0 ? 0L : ratingCounts.getOrDefault(2, 0L) * 100 / totalReviewCount;
+        Long oneRate = totalReviewCount == 0.0 ? 0L : ratingCounts.getOrDefault(1, 0L) * 100 / totalReviewCount;
 
         return new GetWalkwayRatingResponse(avgRating, totalReviewCount, fiveRate, fourRate, threeRate, twoRate, oneRate);
     }
