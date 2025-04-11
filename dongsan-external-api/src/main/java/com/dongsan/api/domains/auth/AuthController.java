11d10522f6cb8a61bd2570a383beade7e8af2200
Yuri Cho / 조유리 <113884028+jorippppong@users.noreply.github.com>
2025-04-11 17:22:52 +0900

    [feature/DB-238]: 탈퇴하기 API, 회원가입 시 프로필 사진 제외 (#72)
    * feat(DB-239): 탈퇴하기 API 구현
    * feat(DB-240): 회원가입 시 프로필 사진 제외

--- dongsan-external-api/src/main/java/com/dongsan/api/domains/auth/AuthController.java
+++ dongsan-external-api/src/main/java/com/dongsan/api/domains/auth/AuthController.java
@@ -1,5 +1,9 @@
 package com.dongsan.api.domains.auth;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -7,41 +11,52 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
 @RestController
 @RequestMapping("/auth")
 @Tag(name = "인증")
 public class AuthController {
-	private final AuthService authService;
+    private final AuthService authService;
 
-	public AuthController(AuthService authService) {
-		this.authService = authService;
-	}
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
 
-	@Operation(summary = "Access token 재발급 (Refresh Token을 받으면 Access token & Refresh Token 재발급, Refresh Token 만료 시 다시"
-		+ " 로그인 진행해야 함)")
-	@PostMapping("/refresh")
-	public ResponseEntity<Void> renewToken(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) {
-		authService.renewToken(request, response);
-		return ResponseEntity.ok()
-			.build();
-	}
+    @Operation(summary = "Access token 재발급 (Refresh Token을 받으면 Access token & Refresh Token 재발급, Refresh Token 만료 시 다시"
+            + " 로그인 진행해야 함)")
+    @PostMapping("/refresh")
+    public ResponseEntity<Void> renewToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        authService.renewToken(request, response);
+        return ResponseEntity.ok().build();
+    }
 
-	@Operation(summary = "로그 아웃")
-	@DeleteMapping("/logout")
-	public ResponseEntity<Void> logout(
-		@AuthenticationPrincipal CustomAuthUser customOAuth2User,
-		HttpServletResponse response
-	) {
-		authService.logout(customOAuth2User.getMemberId(), response);
-		return ResponseEntity.ok()
-			.build();
-	}
+    /**
+     * 우선은 빠른 재심사를 위해 로그아웃과 동일하게 토큰만 삭제하는 걸로 되어 있음.
+     * (개선 방식)
+     * 필드 추가해서 isDeactivated = true, deactivatedAt 설정
+     * 14일 지나면 데이터 아예 삭제 (spring batch 사용)
+     * 14일 내로 돌아오면 탈퇴하지 않은 걸로 다시 활동
+     * 삭제되거나 deactivate 된 사용자는 (알수없음) 으로 닉네임이 뜨도록 설정 필요 (도메인 객체에서 따로 매핑 해야 할듯)
+     */
+    @Operation(summary = "로그 아웃")
+    @DeleteMapping("/logout")
+    public ResponseEntity<Void> logout(
+            @AuthenticationPrincipal CustomAuthUser customOAuth2User,
+            HttpServletResponse response
+    ) {
+        authService.logout(customOAuth2User.getMemberId(), response);
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "탈퇴하기")
+    @DeleteMapping("/deactivate")
+    public ResponseEntity<Void> deactivate(
+            @AuthenticationPrincipal CustomAuthUser customAuthUser,
+            HttpServletResponse response
+    ) {
+        authService.logout(customAuthUser.getMemberId(), response);
+        return ResponseEntity.ok().build();
+    }
 }
--- dongsan-external-api/src/main/java/com/dongsan/api/domains/auth/oauth2/OAuth2Attributes.java
+++ dongsan-external-api/src/main/java/com/dongsan/api/domains/auth/oauth2/OAuth2Attributes.java
@@ -1,31 +1,29 @@
 package com.dongsan.api.domains.auth.oauth2;
 
-import java.util.Map;
-
 import com.dongsan.core.domains.auth.Provider;
 
+import java.util.Map;
+
 public record OAuth2Attributes(
-	String email,
-	String nickname,
-	String profileImage
+        String email,
+        String nickname
 ) {
 
-	public static OAuth2Attributes of(Provider socialType, Map<String, Object> attributes) {
-		return switch (socialType) {
-			case KAKAO -> ofKakao(attributes);
-			case NAVER -> ofNaver(attributes);
-		};
-	}
+    public static OAuth2Attributes of(Provider socialType, Map<String, Object> attributes) {
+        return switch (socialType) {
+            case KAKAO -> ofKakao(attributes);
+            case NAVER -> ofNaver(attributes);
+        };
+    }
 
-	private static OAuth2Attributes ofKakao(Map<String, Object> attributes) {
-		Map<String, Object> kakaoAccount = (Map<String, Object>)attributes.get("kakao_account");
-		Map<String, String> profile = (Map<String, String>)kakaoAccount.get("profile");
-		return new OAuth2Attributes((String)kakaoAccount.get("email"), profile.get("nickname"),
-			profile.get("profile_image_url"));
-	}
+    private static OAuth2Attributes ofKakao(Map<String, Object> attributes) {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, String> profile = (Map<String, String>) kakaoAccount.get("profile");
+        return new OAuth2Attributes((String) kakaoAccount.get("email"), profile.get("nickname"));
+    }
 
-	private static OAuth2Attributes ofNaver(Map<String, Object> attributes) {
-		Map<String, String> response = (Map<String, String>)attributes.get("response");
-		return new OAuth2Attributes(response.get("email"), response.get("nickname"), response.get("profile_image"));
-	}
+    private static OAuth2Attributes ofNaver(Map<String, Object> attributes) {
+        Map<String, String> response = (Map<String, String>) attributes.get("response");
+        return new OAuth2Attributes(response.get("email"), response.get("nickname"));
+    }
 }
