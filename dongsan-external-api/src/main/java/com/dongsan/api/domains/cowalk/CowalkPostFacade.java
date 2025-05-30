9be9aca5119f6ad96825162f5f5c862b41fe0f5b
김건우 <rjswjddn@gmail.com>
2025-05-31 00:39:02 +0900

    feature: 크루 가입 여부 검증 추가

--- dongsan-external-api/src/main/java/com/dongsan/api/domains/cowalk/CowalkPostFacade.java
+++ dongsan-external-api/src/main/java/com/dongsan/api/domains/cowalk/CowalkPostFacade.java
@@ -7,20 +7,27 @@ import com.dongsan.api.domains.cowalk.dto.request.CreateCowalkPostRequest;
 import com.dongsan.domain.domains.cowalk.CreateCowalkPostCommand;
 import com.dongsan.domain.domains.cowalk.service.CowalkParticipantRdbService;
 import com.dongsan.domain.domains.cowalk.service.CowalkPostRdbService;
+import com.dongsan.domain.domains.crew.service.CrewMemberRdbService;
 
 @Service
 @Transactional
 public class CowalkPostFacade {
 	private final CowalkParticipantRdbService cowalkParticipantRdbService;
 	private final CowalkPostRdbService cowalkPostRdbService;
+	private final CrewMemberRdbService crewMemberRdbService;
 
-	public CowalkPostFacade(CowalkParticipantRdbService cowalkParticipantRdbService,
-		CowalkPostRdbService cowalkPostRdbService) {
+	public CowalkPostFacade(
+		CowalkParticipantRdbService cowalkParticipantRdbService,
+		CowalkPostRdbService cowalkPostRdbService,
+		CrewMemberRdbService crewMemberRdbService
+	) {
 		this.cowalkParticipantRdbService = cowalkParticipantRdbService;
 		this.cowalkPostRdbService = cowalkPostRdbService;
+		this.crewMemberRdbService = crewMemberRdbService;
 	}
 
 	public Long saveCowalkPost(CreateCowalkPostRequest createCowalkPostRequest, Long crewId, Long memberId) {
+		crewMemberRdbService.validateIsCrewMember(crewId, memberId);
 		CreateCowalkPostCommand command = createCowalkPostRequest.toCreateCowalkPostCommand(crewId, memberId);
 		Long cowalkPostId = cowalkPostRdbService.save(command);
 		cowalkParticipantRdbService.save(memberId, cowalkPostId);
