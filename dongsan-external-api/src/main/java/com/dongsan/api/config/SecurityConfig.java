fa37cc99f83f87533e63de02e001a93a3629091c
Yuri Cho / 조유리 <113884028+jorippppong@users.noreply.github.com>
2025-06-17 19:19:22 +0900

    [feature/DB-310]: test용 도메인 CORS 추가 (#92)

--- dongsan-external-api/src/main/java/com/dongsan/api/config/SecurityConfig.java
+++ dongsan-external-api/src/main/java/com/dongsan/api/config/SecurityConfig.java
@@ -71,10 +71,11 @@ public class SecurityConfig {
                         "http://localhost:3000",
                         "http://dongsanwalk.site",
                         "http://api.dongsanwalk.site:8080",
+                        "http://front.dongsanwalk.site:3000",
                         "https://dongsanwalk.site",
                         "https://www.dongsanwalk.site",
                         "https://api.dongsanwalk.site",
-                        "http://front.dongsanwalk.site:3000",
+                        "https://testui.dongsanwalk.site",
                         "https://appleid.apple.com"
                 )
         );
