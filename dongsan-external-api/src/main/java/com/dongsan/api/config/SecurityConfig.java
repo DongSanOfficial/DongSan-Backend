363e66b2515ed98a516b106f93e4b465c5386a69
jorippppong <duelee75@gmail.com>
2025-06-27 13:45:42 +0900

    fix: websocket 인증 로직 수정

--- dongsan-external-api/src/main/java/com/dongsan/api/config/SecurityConfig.java
+++ dongsan-external-api/src/main/java/com/dongsan/api/config/SecurityConfig.java
@@ -159,7 +159,8 @@ public class SecurityConfig {
                                 "/",
                                 "/health",
                                 "/dev/**",
-                                "/auth/refresh"
+                                "/auth/refresh",
+                                "/ws/**"
                         )
                         .permitAll()
                         // 이외 요청 모두 jwt 필터를 타도록 설정
