b1fa6b7ab004f15d1f33fd032504e7107577ff50
김건우 <rjswjddn@gmail.com>
2025-08-21 20:10:19 +0900

    feature(DB-359): 산책로 수정 DTO에 조건 추가

--- dongsan-external-api/src/main/java/com/dongsan/api/domains/walkway/dto/request/UpdateWalkwayRequest.java
+++ dongsan-external-api/src/main/java/com/dongsan/api/domains/walkway/dto/request/UpdateWalkwayRequest.java
@@ -1,15 +1,19 @@
 package com.dongsan.api.domains.walkway.dto.request;
 
+import java.util.List;
+
 import com.dongsan.domain.domains.walkway.UpdateWalkwayCommand;
 import com.dongsan.domain.domains.walkway.domain.WalkwayExposeLevel;
-import jakarta.validation.constraints.NotNull;
 
-import java.util.List;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record UpdateWalkwayRequest(
         @NotNull
+        @Size(max = 15, message = "산책로 제목은 15자 이내여야 합니다.")
         String name,
         @NotNull
+        @Size(max = 100, message = "산책로 내용은 100자 이내여야 합니다.")
         String memo,
         @NotNull
         List<String> hashtags,
