3a3a1d1ad452c55b37d2aa8b25e24bf54d086bae
김건우 <rjswjddn@gmail.com>
2025-08-27 06:15:17 +0900

    refactor: 불필요한 주석 제거

--- dongsan-core/domain-rdb/src/main/java/com/dongsan/domain/support/paging/CursorResponse.java
+++ dongsan-core/domain-rdb/src/main/java/com/dongsan/domain/support/paging/CursorResponse.java
@@ -6,12 +6,9 @@ public record CursorResponse<T>(
         List<T> data,
         boolean hasNext
 ) {
-    // size 기반 생성 로직을 추가하고 싶으면 보조 생성자로 작성 가능
     public CursorResponse(List<T> data, int size) {
         this(data.size() > size ? List.copyOf(data.subList(0, size)) : List.copyOf(data),
                 data.size() > size);
     }
-
-    // 기본 생성자는 record가 자동 제공하는 canonical constructor
 }
 
