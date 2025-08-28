3b041c514db278a968303eca7bd4a72fcb3bb0b0
jorippppong <duelee75@gmail.com>
2025-08-28 22:11:28 +0900

    test(DB-372): bookmark 통합테스트 작성

--- dongsan-external-api/src/test/java/com/dongsan/api/domains/member/MemberIntegrationTest.java
+++ dongsan-external-api/src/test/java/com/dongsan/api/domains/member/MemberIntegrationTest.java
@@ -1,6 +1,5 @@
-package com.dongsan.api.domains;
+package com.dongsan.api.domains.member;
 
-import com.dongsan.api.domains.member.MemberProfileResponse;
 import com.dongsan.api.support.IntegrationTest;
 import com.dongsan.api.support.TestAuthHelper;
 import org.junit.jupiter.api.Assertions;
--- dongsan-external-api/src/test/java/com/dongsan/api/support/factory/BookmarkFactory.java
+++ dongsan-external-api/src/test/java/com/dongsan/api/support/factory/BookmarkFactory.java
@@ -1,17 +1,28 @@
 package com.dongsan.api.support.factory;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.dongsan.domain.domains.bookmark.domain.Bookmark;
 import com.dongsan.domain.domains.bookmark.domain.BookmarkRepository;
+import com.dongsan.domain.domains.bookmark.domain.MarkedWalkwayRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Component
 public class BookmarkFactory {
     @Autowired
     private BookmarkRepository bookmarkRepository;
+    @Autowired
+    private MarkedWalkwayRepository markedWalkwayRepository;
 
     public Long save(Long memberId) {
         return bookmarkRepository.save(new Bookmark("test", memberId));
     }
+
+    public Long save(Long memberId, String bookmarkName) {
+        return bookmarkRepository.save(new Bookmark(bookmarkName, memberId));
+    }
+
+    public void includeWalkway(Long bookmarkId, Long walkwayId) {
+        markedWalkwayRepository.includeWalkway(bookmarkId, walkwayId);
+    }
+
 }
